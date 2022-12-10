e0926938f80c4b8f1abfb9fb761014b4e2c7131b
Vipul Bansal <vmbansal2001@gmail.com>
2022-12-10 18:15:42 +0530

    app configure changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: process.env.NODE_ENV === 'production' ? true : false,
   swcMinify: true,
   basePath: '',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Logo from 'src/lib/assets/logo.svg'
+import TestComp from 'src/components/TestComp'
+import Image from 'next/image'
 
 type Props = {}
 
 const Index = (props: Props) => {
   return (
-    <div>index</div>
+    <div className='w-20 h-20'>
+      {/* <Image src={Logo} alt=""/> */}
+      <Logo className='border-2'/>
+      <TestComp/>
+    </div>
   )
 }
 
