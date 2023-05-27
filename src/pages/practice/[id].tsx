a9f67f0dcc4e3cde29e350c4afbd6b62fde5f089
Sandeep Jha <91358961+ft-jsandeep@users.noreply.github.com>
2023-05-27 23:08:43 +0530

    implemented Incremental Static Regeneration in practice/[ide].tsx

--- src/pages/practice/[id].tsx
+++ src/pages/practice/[id].tsx
@@ -25,19 +25,24 @@ export async function getStaticPaths() {
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
+  return { paths, fallback: "blocking" };
 }
 
 // This also gets called at build time
 export async function getStaticProps({ params }: { params: any }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
-  const res: any = await apiCall({
-    key: "fetch_question",
-    params: { question_id: params.id },
-  });
-  const question = res.data;
+  let question = {};
+  let notFound = true;
+  try {
+    const res: any = await apiCall({
+      key: "fetch_question",
+      params: { question_id: params.id },
+    });
+    question = res?.data;
+    notFound = question ? false : true;
+  } catch (error) {}
 
   // Pass post data to the page via props
-  return { props: { question } };
+  return { props: { question }, revalidate: 86400, notFound: notFound };
 }
